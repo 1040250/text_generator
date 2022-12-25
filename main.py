b47714dc7bb21a60b7da4d490129bffa1739ea35
Manuel <80636932+1040250@users.noreply.github.com>
2022-12-25 18:08:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ letter_weights_EN = [89, 15, 26, 39, 126, 20, 19, 53, 69, 2, 8, 45, 25, 71, 73, 
 vowel_weights_EN = [89, 126, 69, 73, 30]
 cons_weights_EN = [15, 26, 39, 20, 19, 53, 2, 8, 45, 25, 71, 19, 1, 64, 64, 91, 11, 19, 1, 17, 1]
 
-def generate_text():
+def main():
     text = ''
     for s in range(sentences):
         sentence = ''                                               # start new sentence
@@ -26,19 +26,7 @@ def generate_text():
         sent_length = max(3, int(np.random.normal(28, 16)))          # sentence length: normal(mean, std)
         word_length_last = 0
         for w in range(sent_length):
-            word, vowel_tracker = '', [1, 1]                        # start new word
-            vowel_start = True if 0.5 > np.random.rand() else False # decide if to start with vowel
-            word_length = max(1, int(np.random.normal(4.9, 2.7)))       # determine word length: normal(mean, std)
-            word_length = word_length + 1 if word_length == 1 and (w == sent_length-1 or word_length_last == 1) else word_length # prevent single-letter word at end of sentence and two in a row
-            word_length_last = word_length
-            cap = True if cap_prob > np.random.rand() and w > 0 and word_length > 3 else False     # determine if word capitalized (extra letter in addition to word length)
-            word += letters[r.randint(0, len(letters) - 1)] if cap else word  # start word with cap if so decided above
-            for l in range(word_length): # pick letters for word (consonants and vowels take turns, one-letter words are vowels)
-                if word_length == 1 or vowel_start:
-                    letter = r.choices(consonants, cons_weights_EN)[0] if l / 2 != l // 2 else r.choices(vowels, vowel_weights_EN)[0]
-                else:
-                    letter = r.choices(consonants, cons_weights_EN)[0] if l / 2 == l // 2 else r.choices(vowels, vowel_weights_EN)[0]
-                word += letter # add letter to word
+            word = build_word(w, sent_length, word_length_last)
             comma = True if comma_prob > np.random.rand() and 2 < w < sent_length - 3 else False # determine if comma (not to close to beginning or end)
             sentence += word + ', ' if comma else word + ' ' # add word to sentence (and comma if applicable)
         sentence = (sentence + '.').replace(' .', '. ') if 0.05 < np.random.rand() else (sentence + '?').replace(' ?', '? ') # determine if exclamation point
@@ -46,5 +34,26 @@ def generate_text():
 
     print(text)
 
+def build_word(w, sent_length, word_length_last):
+    # start new word
+    word = ''
+    # decide if to start with vowel
+    vowel_start = True if 0.5 > np.random.rand() else False
+    # determine word length: normal(mean, std)
+    word_length = max(1, int(np.random.normal(4.9, 2.7)))
+    # prevent single-letter word at end of sentence and two in a row
+    word_length = word_length + 1 if word_length == 1 and (w == sent_length-1 or word_length_last == 1) else word_length
+    word_length_last = word_length
+    # determine if word capitalized (extra letter in addition to word length)
+    cap = True if cap_prob > np.random.rand() and w > 0 and word_length > 3 else False
+    word += letters[r.randint(0, len(letters) - 1)] if cap else word  # start word with cap if so decided above
+    for l in range(word_length): # pick letters for word (consonants and vowels take turns, one-letter words are vowels)
+        if word_length == 1 or vowel_start:
+            letter = r.choices(consonants, cons_weights_EN)[0] if l / 2 != l // 2 else r.choices(vowels, vowel_weights_EN)[0]
+        else:
+            letter = r.choices(consonants, cons_weights_EN)[0] if l / 2 == l // 2 else r.choices(vowels, vowel_weights_EN)[0]
+        word += letter # add letter to word
+    return word
+
 if __name__ == '__main__':
-    generate_text()
+    main()
